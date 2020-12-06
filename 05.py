1a178692864353061e04f45d87e3e86c34899530
nurulsucisawaliah <71654996+nurulsucisawaliah@users.noreply.github.com>
2020-12-06 16:25:48 +0800

    Create 05.py

---
+++ 05.py
@@ -0,0 +1,12 @@
+def kuadrat(bil):
+    bilangan = []
+    for i in range(len(bil)):
+        pangkat2_kali = bil[i] * bil[i]
+        bilangan.append(pangkat2_kali)
+    return bilangan
+
+listnya = [2,4,5,6]
+hasil = kuadrat(listnya)
+print('bil' + str(listnya))
+print('hasil = kuadrat(bil)')
+print('sehingga isi dari hasil berupa list dalam bentuk' + str(hasil))

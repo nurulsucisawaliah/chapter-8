03140ee4e21e4383aadec4a382401dd90b9d55fe
nurulsucisawaliah <71654996+nurulsucisawaliah@users.noreply.github.com>
2020-12-06 19:03:53 +0800

    Update 09.py

--- 09.py
+++ 09.py
@@ -6,15 +6,13 @@ buah = {'Apel'  : 5000,
 fruit_name = list(buah)
 fruit_harga = list(buah.values())
 
-print("input-lah nama buah yang ingin dibeli")
-beli_buah = input('Nama buah yang ingin dibeli : ')
-
-print("input-lah berapa kg buah yang ingin dibeli")
-kilo_gram = int(input('Berapa kg : '))
-
+print("input-lah nama buah yang ingin dibeli dan berapa kg buah yang ingin dibeli")
+beli_buah = input('Nama buah yang  dibeli : ')
+kilo_gram = int(input('Berapa kg              : '))
+print('------------------------------------')
 harga_index = fruit_name.index(beli_buah)
 fruit_beli = fruit_harga[harga_index] * kilo_gram 
-print('total harga dari pembelian buah' + str(kilo_gram) + ' adalah ' + str(fruit_beli) )
+print('total harga            :' + str(kilo_gram) +' /'  + 'kg')
 
 
 
